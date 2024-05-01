dce7f683b37b53b40689be978a5c91964fc0e495
Kelly <kellydangora@gmail.com>
2024-05-01 17:08:12 -0400

    added review forms for each planet

---
+++ server/src/db/migrations/20240501141059_createReviews.cjs
@@ -0,0 +1,23 @@
+/**
+ * @typedef {import("knex")} Knex
+ */
+
+/**
+ * @param {Knex} knex
+ */
+exports.up = async (knex) => {
+  return knex.schema.createTable("reviews", (table) => {
+      table.bigIncrements("id")
+      table.string("reviewText").notNullable()
+      table.bigInteger("planetId").unsigned().notNullable().index().references("planets.id")
+      table.timestamp("createdAt").notNullable().defaultTo(knex.fn.now())
+      table.timestamp("updatedAt").notNullable().defaultTo(knex.fn.now())
+  })
+};
+
+/**
+* @param {Knex} knex
+*/
+exports.down = async (knex) => {
+  return knex.schema.dropTableIfExists("reviews")
+};
--- server/src/models/index.js
+++ server/src/models/index.js
@@ -1,5 +1,6 @@
 // include all of your models here using CommonJS requires
 const User = require("./User.js");
 const Planet = require("./Planet.js")
+const Review = require("./Review.js")
 
-module.exports = { User, Planet };
+module.exports = { User, Planet, Review };
